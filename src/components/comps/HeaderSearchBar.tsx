def385cbfe4be1ae3570e2d55f2e365f82801302
Adefiance <1strategic.breakthrough@gmail.com>
2025-06-05 07:02:55 +0100

    Changes

--- src/components/comps/HeaderSearchBar.tsx
+++ src/components/comps/HeaderSearchBar.tsx
@@ -2,7 +2,10 @@
 
 function HeaderSearchBar () {
     return (
-        <div className="w-[55%] h-full bg-[green]">
+        <div className="w-[55%] h-full bg-[green] flex justify-center items-center">
+            <div className="w-[80%] h-[80%] bg-gray-light rounded-[40px]">
+
+            </div>
         </div>
     )
 }
